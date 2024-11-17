5fb54e1070ca20c09edfd445cd4240247d6aad42
Richard Findlay <rfindlay1992@gmail.com>
2024-11-17 17:39:32 +0000

    updated modules list in settings

--- donder/settings.py
+++ donder/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
-import dj_database_url
 from environ import Env 
 from pathlib import Path
 
