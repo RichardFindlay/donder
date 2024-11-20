da33c6f50fea0faa45a9a09c09cb541aae3a399a
Richard Findlay <rfindlay1992@gmail.com>
2024-11-20 20:49:16 +0000

    settings update

--- donder/settings.py
+++ donder/settings.py
@@ -242,7 +242,7 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
     # # STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
     # S3_BUCKET_NAME = env('S3_BUCKET_NAME')
